77ec45ed5cdefed63f812e96841e3b497e293c23
Vicente Estrada <201112007@alumnos.utn.edu.mx>
2021-10-22 18:18:36 -0500

    Crud Completo

--- Controllers/ModuleController.cs
+++ Controllers/ModuleController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MVCAPIAuthenticationTecsaUser.Moldels;
 using MVCAPIAuthenticationTecsaUser.Models.Response;
+using MVCAPIAuthenticationTecsaUser.Models.Request;
 
 namespace MVCAPIAuthenticationTecsaUser.Controllers
 {
@@ -33,6 +34,74 @@ namespace MVCAPIAuthenticationTecsaUser.Controllers
 
                 return Ok(oAnswer);
             }
+        }
+        [HttpPost]
+        public IActionResult AddModules(ModuleRequest oModel)
+        {
+            Answer oAnswer = new Answer();
+            oAnswer.Successful = 0;
+            try
+            {
+                using(tecsaofficeContext db = new tecsaofficeContext())
+                {
+                    Module oModule = new Module();
+                    oModule.NameModule = oModel.Name_module;
+                    db.Modules.Add(oModule);
+                    db.SaveChanges();
+                    oAnswer.Successful = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oAnswer.Message = ex.Message;
+            }
+
+            return Ok(oAnswer);
+        }
+        [HttpPut]
+        public IActionResult UpDateModule(ModuleRequest oModel)
+        {
+            Answer oAnswer = new Answer();
+            oAnswer.Successful = 0;
+            try
+            {
+                using (tecsaofficeContext db = new tecsaofficeContext())
+                {
+                    Module oModule = db.Modules.Find(oModel.Id_module);
+                    oModule.NameModule = oModel.Name_module;
+                    db.Entry(oModule).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    oAnswer.Successful = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oAnswer.Message = ex.Message;
+            }
+
+            return Ok(oAnswer);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DelateModule(int id)
+        {
+            Answer oAnswer = new Answer();
+            oAnswer.Successful = 0;
+            try
+            {
+                using (tecsaofficeContext db = new tecsaofficeContext())
+                {
+                    Module oModule = db.Modules.Find(id);
+                    db.Remove(oModule);
+                    db.SaveChanges();
+                    oAnswer.Successful = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                oAnswer.Message = ex.Message;
+            }
+
+            return Ok(oAnswer);
         }
     }
 }
---
+++ Models/Request/ModuleRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAPIAuthenticationTecsaUser.Models.Request
+{
+    public class ModuleRequest
+    {
+        public int Id_module { get; set; }
+        public string Name_module { get; set; }
+    }
+}
