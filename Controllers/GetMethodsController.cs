77a3ce9362394486103ba11f975c9a919862d93e
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2021-10-29 18:25:33 -0500

    Agregado controlador de metodos consulta, faltante 9 metodos

---
+++ Controllers/GetMethodsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCAPIAuthenticationTecsaUser.Moldels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAPIAuthenticationTecsaUser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetMethodsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetOpeationModule()
+        {
+            try
+            {
+                using(tecsaofficeContext db = new tecsaofficeContext())
+                {
+                    var operationModule = db.Operations.Join(
+                        db.Modules,
+                        ope => ope.IdModule,
+                        mod => mod.IdModule,
+                        (ope,mod) =>
+                            new { ope.IdOperation, ope.NameOperation, mod.NameModule }
+                        ).ToList();
+
+                    return Ok(operationModule);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
